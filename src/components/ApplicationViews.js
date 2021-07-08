e694610c8196aab609b0b313ef5f2f295e845d4c
dparsley1203 <dparsley1203@gmail.com>
2021-07-08 14:56:31 -0500

    added animals React refactor

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
+import { CustomerCard } from "./customer/CustomerCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
+import { LocationCard } from "./location/LocationCard"
+
+export const ApplicationViews = () => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <AnimalProvider>
+                <Route exact path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
+
+            <Route path="/customers">
+                <CustomerCard />
+            </Route>
+
+            <Route path="/employees">
+                <EmployeeCard />
+            </Route>
+
+            <Route path="/locations">
+                <LocationCard />
+            </Route>
+        </>
+    )
+}
